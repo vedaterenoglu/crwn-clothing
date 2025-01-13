c137b6c0a38b5de7db64c154b09a3bf746f31376
gtbs-coding <gtbs.se@gmail.com>
2025-01-13 14:21:13 +0100

    addToCart feature is added

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,13 +1,21 @@
 /** @format */
 
+import { useContext } from 'react'
+import { CartContext } from '../../contexts/cart.context'
 import Button from '../button/button.component'
+import CartItem from '../cart-item/cart-item.component'
 
 import './cart-dropdown.styles.scss'
 
 const CartDropdown = () => {
+	const { cartItems } = useContext(CartContext)
 	return (
 		<div className='cart-dropdown-container'>
-			<div className='cart-items'></div>
+			<div className='cart-items'>
+		    {cartItems.map((cartItem) => (
+					<CartItem key={cartItem.id} cartItem={cartItem} />
+				))}
+			</div>
 			<Button buttonType='inverted'>Checkout</Button>
 		</div>
 	)
